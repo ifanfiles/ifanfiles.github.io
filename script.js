79c418a6dabcca52a4cbb6e6a7fbdba3d212c7e8
ifanfiles <idanafi@gmail.com>
2025-08-30 22:30:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+// Tambah layer radar dari RainViewer (real-time)
+var radarLayer = L.tileLayer(
+  'https://tilecache.rainviewer.com/v2/radar/256/{z}/{x}/{y}/2/1_1.png',
+  { attribution: 'Radar data © RainViewer.com' }
+).addTo(map);
+
 // Load batas wilayah dari GeoJSON
 fetch('batas_wilayah.geojson')
   .then(response => response.json())
@@ -17,5 +23,6 @@ fetch('batas_wilayah.geojson')
     // ⬇️ Zoom otomatis sesuai batas wilayah
     map.fitBounds(batas.getBounds());
   });
+
 
 
