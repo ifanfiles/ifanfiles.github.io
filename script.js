7cd0c36db47fa9e2b71b1c3e3ba44cb592f96b20
ifanfiles <idanafi@gmail.com>
2025-08-30 21:57:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Inisialisasi map di lokasi Jawa Tengah
+var map = L.map('map').setView([-7.4, 110], 8);
+
+// Basemap OSM (wajib ada agar Windy overlay terlihat)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Tambahkan overlay dari Windy (contoh radar)
+var windyRadar = L.tileLayer(
+  'https://tilecache.rainviewer.com/v2/radar/{z}/{x}/{y}/1/1_1.png', 
+  { attribution: 'Radar data © Windy.com' }
+).addTo(map);
+
+// Tambahkan batas wilayah dari GeoJSON
+fetch('batas_wilayah.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: { color: "red", weight: 2, fillOpacity: 0 }
+    }).addTo(map);
+  });
